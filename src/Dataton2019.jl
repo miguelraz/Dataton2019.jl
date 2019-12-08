d118a9c399d5d93157fe9ff638b61ead89e4782a
miguelraz <miguelraz@ciencias.unam.mx>
2019-12-08 16:13:22 -0600

    reducir JSON

--- src/Dataton2019.jl
+++ src/Dataton2019.jl
@@ -17,60 +17,62 @@ function __init__()
         "",
     ));
 
-    register(DataDep("icijoffshore2018",
-        """
-        Dataset: ICIJ Offshore Leaks 2018 CSV
-        Author: ICIJ
-        License: Open
-        Website: https://offshoreleaks.icij.org/
-
-
-        Notice: this file is over 100MB
-        """,
-        "https://offshoreleaks-data.icij.org/offshoreleaks/csv/csv_offshore_leaks.2018-02-14.zip",
-        "05e7a0382ce9e0b9fe17b20d332e84f33fcf8c381a86e116de00bafbe5036043",
-    ));
-
-    register(DataDep("icijpanamapapers2018",
-        """
-        Dataset: ICIJ Offshore Leaks CSV
-        Author: ICIJ
-        License: Open
-        Website: https://offshoreleaks.icij.org/
-
-
-        Notice: this file is over 100MB
-        """,
-        "https://offshoreleaks-data.icij.org/offshoreleaks/csv/csv_panama_papers.2018-02-14.zip",
-        "",
-    ));
-
-    register(DataDep("icijparadise2018",
-        """
-        Dataset: ICIJ Offshore Leaks CSV
-        Author: ICIJ
-        License: Open
-        Website: https://offshoreleaks.icij.org/
-
-
-        Notice: this file is over 100MB
-        """,
-        "https://offshoreleaks-data.icij.org/offshoreleaks/csv/csv_paradise_papers.2018-02-14.zip",
-        "",
-    ));
-    # 
-    # register(DataDep("s6",
+    # Para usar estos datos, descomentar estas lineas.
+    #
+    # register(DataDep("icijoffshore2018",
     #     """
-    #     Dataset: s6contrataciones
-    #     Author: GOBMEX
+    #     Dataset: ICIJ Offshore Leaks 2018 CSV
+    #     Author: ICIJ
     #     License: Open
-    #     Website:
+    #     Website: https://offshoreleaks.icij.org/
+    #
     #
     #     Notice: this file is over 100MB
     #     """,
-    #     "https://drive.google.com/uc?export=download&confirm=ezN3&id=1XOYDLVv-RqcMs8_hzkZ0fjC9FYh2psFw",
+    #     "https://offshoreleaks-data.icij.org/offshoreleaks/csv/csv_offshore_leaks.2018-02-14.zip",
+    #     "05e7a0382ce9e0b9fe17b20d332e84f33fcf8c381a86e116de00bafbe5036043",
+    # ));
+    #
+    # register(DataDep("icijpanamapapers2018",
+    #     """
+    #     Dataset: ICIJ Offshore Leaks CSV
+    #     Author: ICIJ
+    #     License: Open
+    #     Website: https://offshoreleaks.icij.org/
+    #
+    #
+    #     Notice: this file is over 100MB
+    #     """,
+    #     "https://offshoreleaks-data.icij.org/offshoreleaks/csv/csv_panama_papers.2018-02-14.zip",
     #     "",
     # ));
+
+    # register(DataDep("icijparadise2018",
+    #     """
+    #     Dataset: ICIJ Offshore Leaks CSV
+    #     Author: ICIJ
+    #     License: Open
+    #     Website: https://offshoreleaks.icij.org/
+    #
+    #
+    #     Notice: this file is over 100MB
+    #     """,
+    #     "https://offshoreleaks-data.icij.org/offshoreleaks/csv/csv_paradise_papers.2018-02-14.zip",
+    #     "",
+    # ));
+    #
+    register(DataDep("s6mini",
+         """
+         Dataset: s6contratacionesmini
+         Author: GOBMEX, modified by Miguel Raz
+         License: Open
+         Website:
+
+         Notice: this file is over 20MB
+         """,
+         "https://raw.githubusercontent.com/miguelraz/Dataton2019.jl/master/test/mini.json",
+         "",
+    ));
 
 
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,32 +3,43 @@ using Test
 using DataDeps, DataFrames, LightGraphs, CSV, ZipFile, JSON
 
 @info "Hola Dataton2019!"
-foo(x) = x^2
 @testset "Datos" begin
     #global t1 = readdir(datadep"icijparadise2018")[1]
     #@test  "csv_paradise_papers.2018-02-14.zip" == t1
     #global t2 = readdir(datadep"icijpanamapapers2018")[1]
     #@test "csv_panama_papers.2018-02-14.zip" == t2
-    #global t3 = readdir(datadep"icijoffshore2018")[1]
-    #@test  "csv_offshore_leaks.2018-02-14.zip" == t3
-    global t4 = readdir(datadep"icijbahamas2017")[1]
-    @test  "csv_bahamas_leaks.2017-12-19.zip" == t4
+    global t3 = readdir(datadep"icijoffshore2018")[1]
+    @test  "csv_offshore_leaks.2018-02-14.zip" == t3
+    #global t4 = readdir(datadep"icijbahamas2017")[1]
+    #@test  "csv_bahamas_leaks.2017-12-19.zip" == t4
 
     # Esta es una version mini de el archivo "contrataciones_arr.json"
     # Encontrado en https://github.com/pdnmx/datosdataton2019
     # Minimizamos el archivo con el comando 'split -n 100 contartaciones_arr.json'
     # y cambiamos el nombre de 'xaa' a 'mini.json'
-    filejson = open("contrataciones_arr.json", "r")
+    #
+    # Encontramos la ultima entrada que se ha recortado y le pegamos un "]"
+    #
+    x = readlines(open("mini.json"))[1];
+    #
+    # julia> findlast("{\"_id\"",x)
+    # 22869056:22869061
+    #
+    s6 = JSON.parse(x[1:22869054] * "]");
 
-    global s6 = filejson |> JSON.parse |> DataFrame
+    @test length(s6) == 6462
 
 end
 
+# Hacemos el unzip del archivo de bahamas2017
+
 #pars2018 = ZipFile.Reader(datadep"icijparadise2018" * "/" * t1)
 #panama2018 = ZipFile.Reader(datadep"icijpanamapapers2018" * "/" * t2)
-#offshore2018 = ZipFile.Reader(datadep"icijoffshore2018" * "/" * t3)
-bahamas2017 = ZipFile.Reader(datadep"icijbahamas2017" * "/" * t4)
-#
+offshore2018 = ZipFile.Reader(datadep"icijoffshore2018" * "/" * t3)
+#bahamas2017 = ZipFile.Reader(datadep"icijbahamas2017" * "/" * t4)
+
+# Este es el formato de los archivos a investigar
+
 # uncompressedsize method  mtime            name
 # ----------------------------------------------
 #        234614743 Deflate 2018-02-13 17-44 paradise_papers.edges.csv
@@ -49,17 +60,19 @@ bahamas2017 = ZipFile.Reader(datadep"icijbahamas2017" * "/" * t4)
 # panama2018interm = CSV.read(panama2018.files[3]; threaded = true)
 # panama2018officer = CSV.read(panama2018.files[4]; threaded = true)
 
-s6 = JSON.parsefile(s6)
+
+# bahamas2017address = CSV.read(bahamas2017.files[1]; threaded = true)
+# bahamas2017entity = CSV.read(bahamas2017.files[2]; threaded = true)
+# bahamas2017interm = CSV.read(bahamas2017.files[3]; threaded = true)
+# bahamas2017officer = CSV.read(bahamas2017.files[4]; threaded = true)
+
+# Extraemos a CSVs
 
 offshore2018address = CSV.read(offshore2018.files[1]; threaded = true)
 offshore2018entity = CSV.read(offshore2018.files[2]; threaded = true)
 offshore2018interm = CSV.read(offshore2018.files[3]; threaded = true)
 offshore2018officer = CSV.read(offshore2018.files[4]; threaded = true)
 
-# bahamas2017address = CSV.read(bahamas2017.files[1]; threaded = true)
-# bahamas2017entity = CSV.read(bahamas2017.files[2]; threaded = true)
-# bahamas2017interm = CSV.read(bahamas2017.files[3]; threaded = true)
-# bahamas2017officer = CSV.read(bahamas2017.files[4]; threaded = true)
 
 occursin.(offshore2018entity[:address], "Mexico") |> sum
 occursin.(offshore2018entity[:address], "México") |> sum
